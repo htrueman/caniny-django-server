2775d139f5ce2a70a28a73926b2c181e8485e20d
Vadym Hevlich <vege1wgw@gmail.com>
2019-04-19 19:00:11 +0300

    OrganizationViewSet permissions fixed

--- apps/organizations/views.py
+++ apps/organizations/views.py
@@ -1,4 +1,5 @@
 from rest_framework import viewsets
+from rest_framework.permissions import AllowAny
 
 from organizations.models import Organization
 from .serializers import OrganizationSerializer
@@ -7,3 +8,8 @@ from .serializers import OrganizationSerializer
 class OrganizationViewSet(viewsets.ModelViewSet):
     serializer_class = OrganizationSerializer
     queryset = Organization.objects.all()
+
+    def get_permissions(self):
+        if self.action == 'list':
+            return [AllowAny(), ]
+        return super().get_permissions()
