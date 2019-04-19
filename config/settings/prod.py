cf4b7027e3977a5813be0615925c15ede6cd7aba
Vadym Hevlich <vege1wgw@gmail.com>
2019-04-19 16:36:53 +0300

    prod settings fix

--- config/settings/prod.py
+++ config/settings/prod.py
@@ -7,3 +7,6 @@ EMAIL_HOST_PASSWORD = 'pass12pass'
 EMAIL_HOST_USER = 'nastavnyk.service@gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
+
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+HOST_NAME = 'http://165.22.152.38:8000'
