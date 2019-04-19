75ea61a1bb8a2921cb40ba282a1f047c136acec9
Vadym Hevlich <vege1wgw@gmail.com>
2019-04-19 16:26:03 +0300

    temp production settings added

---
+++ config/settings/prod.py
@@ -0,0 +1,9 @@
+from .base import *
+
+DEBUG = True
+
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_PASSWORD = 'pass12pass'
+EMAIL_HOST_USER = 'nastavnyk.service@gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
