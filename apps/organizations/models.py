b8be1b711da3b088e43348056025f70678f41836
Vadym Hevlich <vege1wgw@gmail.com>
2019-04-24 21:01:02 +0300

    Organization name unique=True

--- apps/organizations/models.py
+++ apps/organizations/models.py
@@ -2,7 +2,10 @@ from django.db import models
 
 
 class Organization(models.Model):
-    name = models.CharField(max_length=256)
+    name = models.CharField(
+        max_length=256,
+        unique=True
+    )
 
     def __str__(self):
         return 'id: {}, name: {}'.format(self.id, self.name)
