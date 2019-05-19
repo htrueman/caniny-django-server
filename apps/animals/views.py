d69dfaa6a21fbf7b21fe6c6df83d9839b5fa7cf1
Vadym Hevlich <vege1wgw@gmail.com>
2019-05-19 20:23:10 +0300

    age_bound_filter fixed

--- apps/animals/views.py
+++ apps/animals/views.py
@@ -66,6 +66,7 @@ class AnimalFilter(filters.FilterSet):
 
     def age_bound_filter(self, queryset, age, value):
         left_bound_date_of_birth = datetime.datetime.now() - relativedelta(years=+value)
+        print(left_bound_date_of_birth, value)
         if 'gte' in age:
             return queryset.filter(Q(age__gte=value) | Q(date_of_birth__gte=left_bound_date_of_birth))
         else:
