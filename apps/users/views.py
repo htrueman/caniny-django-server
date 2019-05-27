54d299115c7c769d7c6ca8bdf00f1aa19601dd8d
Vadym Hevlich <vege1wgw@gmail.com>
2019-05-27 22:55:31 +0300

    UserViewSet get_permissions fixed

--- apps/users/views.py
+++ apps/users/views.py
@@ -192,6 +192,9 @@ class UserViewSet(BulkDeleteMixin, viewsets.ModelViewSet):
     def get_permissions(self):
         if self.action in ('destroy', 'update', 'partial_update', 'change_password', 'bulk_delete',):
             return [user_permissions.SuperAdminPermission()]
+        elif self.action in ('destroy', 'update', 'partial_update', 'change_password',) \
+                and self.request.user == self.get_object():
+            return super().get_permissions()
         return super().get_permissions()
 
     def get_queryset(self, ids=None):
