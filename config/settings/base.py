9ed0db396802385531c605f76b77c32f2d59ba8f
Vadym Hevlich <vege1wgw@gmail.com>
2019-05-29 13:24:30 +0300

    static dirs upd

--- config/settings/base.py
+++ config/settings/base.py
@@ -116,10 +116,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, '../static')
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = os.path.join(BASE_DIR, '../media')
 
 
 # Auth
