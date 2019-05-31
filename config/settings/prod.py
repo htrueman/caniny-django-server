364d1ce166491a1833e3cd065baf1f3a81827fc4
Vadym Hevlich <vege1wgw@gmail.com>
2019-05-31 18:00:20 +0300

    prod settings

--- config/settings/prod.py
+++ config/settings/prod.py
@@ -3,10 +3,12 @@ from .base import *
 DEBUG = True
 
 EMAIL_HOST = 'smtp.sendgrid.net'
-EMAIL_HOST_USER = 'Nexu'
-EMAIL_HOST_PASSWORD = 'vege12ve'
+EMAIL_HOST_USER = 'apikey'  # this is exactly the value 'apikey'
+EMAIL_HOST_PASSWORD = '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'  # this is your API key
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+DEFAULT_FROM_EMAIL = 'no-reply@caniny.com'  # this is the sendgrid email
 
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 HOST_NAME = 'http://165.22.152.38'
